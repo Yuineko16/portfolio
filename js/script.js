9c2a1f8c1a315fe7f6ba7b0355e25cea5a606fae
Yuineko16 <72602795+Yuineko16@users.noreply.github.com>
2021-01-31 21:05:42 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ $(function () {
     $(window).on('load', function () {
         loader.fadeOut();
     });
-    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+    //ページの読み込みが完了してなくても4秒後にアニメーションを非表示にする
     setTimeout(function () {
         loader.fadeOut();
-    }, 3000);
+    }, 4000);
 
 
     /*-----------------------------------------------------------------------
@@ -18,21 +18,21 @@ $(function () {
     var dis = 300;  //スライドメニューによる移動距離
     var device_width = $(window).width();
     var device_mobile = 900;
-    var menu_active = 1;    //1のとき閉じた状態、-1のとき開いた状態
+    var menu_close = 1;    //1のとき閉じた状態、-1のとき開いた状態
 
     $(window).resize(function () {
         //画面の大きさを変更した場合の処理
         if (window.matchMedia('(min-width:' + device_mobile + 'px)').matches) {
             //PC用
             $(".main-nav").css("left", "0");
-            if (menu_active == 1) {
+            if (menu_close == 1) {
                 //メニューが閉じているとき
                 $(".main-nav").css("margin-left", "-300px");
             }
         }
         else {
             //スマホ用
-            if (menu_active == 1) {
+            if (menu_close == 1) {
                 //メニューが閉じているとき
                 $(".main-nav").css("margin-left", "-100%");
             }
@@ -48,13 +48,13 @@ $(function () {
     $(".menu-button").click(function () {
         /*スライドメニューの実装(画面幅によってメニューの表示を変更する)*/
         if (window.matchMedia('(min-width:' + device_mobile + 'px)').matches) {
-            $(".main-nav").animate({ "margin-left": "+=" + dis * menu_active + "px" }, 200);
-            menu_active *= -1;
+            $(".main-nav").animate({ "margin-left": "+=" + dis * menu_close + "px" }, 200);
+            menu_close *= -1;
             $(this).toggleClass('active');
         }
         else {
-            $(".main-nav").animate({ "margin-left": "+=" + device_width * menu_active + "px" }, 200);
-            menu_active *= -1;
+            $(".main-nav").animate({ "margin-left": "+=" + device_width * menu_close + "px" }, 200);
+            menu_close *= -1;
             $(this).toggleClass('active');
         }
     });
@@ -74,5 +74,36 @@ $(function () {
             $('body,html').animate({ scrollTop: pos - 10 + "px" }, 1000);
         }
         return false;
+    });
+
+    /*-----------------------------------------------------------------------
+   要素のフェードインのアニメーション*/
+    $(window).on('load scroll',function (){
+		$('.topic').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+            var height = $(window).height();
+            //フェードインしているかどうかの判定
+            var isFeedin = false;
+
+			//ターゲットまでスクロールするとフェードインする
+			if ((scroll > target - height + 200) && (isFeedin==false)){
+				//クラスを付与
+                $(this).addClass('active');
+                isFeedin = true;
+            }
+
+            //ターゲットを超えてスクロールするとフェードアウトする
+            if ((scroll < target - height -200)||((scroll > target + 550)) && isFeedin){
+                //クラスを外す
+                console.log("topic");
+                $(this).removeClass('active');
+                isFeedin = false;
+            }   
+            
+		});
     });
 })
